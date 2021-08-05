63e30b916cabb18c2d00a29834610d647fe568a6
josegabjimenez <jose.vidales@correounivalle.edu.co>
2021-08-05 11:22:08 -0500

    Estructura creada

---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './styles/Header.scss';
+
+const Header = () => {
+    return (
+        <header>
+            <nav>
+                <ul>
+                    <li>Inicio</li>
+                    <li>¿Quienes somos?</li>
+                    <li>Donde estamos</li>
+                    <li>Contacto</li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default Header
---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../components/Header';
+import './styles/Home.scss';
+
+const Home = () => {
+    return (
+        <main>
+            <Header />
+        </main>
+    )
+}
+
+export default Home
