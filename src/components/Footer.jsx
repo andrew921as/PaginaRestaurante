b9d4518049150c0522c305f51aab96bf74f1e811
andrew <andresfelipecoronado@gmail.com>
2021-08-09 16:08:18 -0500

    Fotter al 90%

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -13,25 +13,27 @@ const Footer = () => {
                     <img src={LogoK}/>
                 </div>
                 {/* Column 2 */}
-                <div className="menu">
-                    <h4>NAVEGACIÓN</h4>
-                    <br />
-                    <ul>
-                        <li>Inicio</li>
-                        <li>Somos Natural</li>
-                        <li>¿Dónde estamos?</li>
-                        <li>Contacto</li>
-                    </ul>
-                    
-                </div>
-                {/* Column 3 */}
-                <div className="follow">
-                <h4>SÍGUENOS EN</h4>
-                    <ul>
-                        <li><img src={LogoFace}/><img src={LogoInsta}/></li>
+                <div className="texto">
+                    <div className="menu">
+                        <h4>NAVEGACIÓN</h4>
                         <br />
-                        <li>Ⓒ Copyright 2021<br></br> Todos los derechos reservados</li>
-                    </ul>
+                        <ul>
+                            <li>Inicio</li>
+                            <li>Somos Natural</li>
+                            <li>¿Dónde estamos?</li>
+                            <li>Contacto</li>
+                        </ul>
+                    
+                    </div>
+                    {/* Column 3 */}
+                    <div className="follow">
+                        <h4>SÍGUENOS EN</h4>
+                        <ul>
+                            <li><img src={LogoFace}/><img src={LogoInsta}/></li>
+                            <br />
+                            <li>Ⓒ Copyright 2021<br></br> Todos los derechos reservados</li>
+                        </ul>
+                    </div>
                 </div>
 
             </div> 
