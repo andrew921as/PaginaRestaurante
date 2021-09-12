87d84855892aca0b45af3867a425cfd84dca6d2a
andrew <andresfelipecoronado@gmail.com>
2021-09-12 18:46:55 -0500

    paginax

--- src/components/Formulary.jsx
+++ src/components/Formulary.jsx
@@ -4,7 +4,7 @@ import './styles/Formulary.scss';
 const Formulary = () => {
     return(
         <div className="GoogleForm">
-            <a href='https://docs.google.com/forms/d/e/1FAIpQLSf_6ln1dMeGwEJ1A2YPcw1MCZULJ41ByiOcOMww6uXtftFIoQ/viewform?usp=sf_link' target="_blank" rel="noreferrer" className="Link"> Dejanos tu opinión </a>
+            <a href='https://formulario.karenspizza.com/' target="_blank" rel="noreferrer" className="Link"> Dejanos tu opinión </a>
         </div>
     )
 }
