8dfe274263e5f4076cedddf04300e3df35af7a07
Andres Velasco <andresfelipecoronado@gmail.com>
2023-02-27 17:24:36 -0500

    Se actualizo el menú

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -5,8 +5,6 @@ import Menu1Esp from '../images/Menu/Portada1Esp.jpg';
 import Menu2Esp from '../images/Menu/Portada2Esp.jpg';
 import Menu1Eng from '../images/Menu/Portada1English.jpg';
 import Menu2Eng from '../images/Menu/Portada2English.jpg';
-import menuEsp from '../images/Menu/menuEsp.pdf';
-import menuEng from '../images/Menu/menuEng.pdf';
 
 
 const Menu = () => {
@@ -16,12 +14,12 @@ const Menu = () => {
         <div className="containerM">
             <h3>Menú</h3>
             <div className="imagenes">
-                <a href={menuEsp} target="_blank" rel="noreferrer">
+                <a href={'https://app.menupp.co/menu/karenspizza'} target="_blank" rel="noreferrer">
                     <img style={{objectPosition: "center"}} src={Menu2Esp} alt="Menu1"/>
                     <img className="segundaI" style={{objectPosition: "center"}} src={Menu1Esp} alt="Menu1 seleccionado"/>
                 </a>
 
-                <a href={menuEng} target="_blank" rel="noreferrer">
+                <a href={'https://app.menupp.co/restaurant/karenspizza/menu/Z0qXgdv0s1Efxpq291Lf'} target="_blank" rel="noreferrer">
                     <img style={{objectPosition: "center"}} src={Menu2Eng} alt="Menu1"/>
                     <img className="segundaI" style={{objectPosition: "center"}} src={Menu1Eng} alt="Menu1 seleccionado"/>
                 </a>
